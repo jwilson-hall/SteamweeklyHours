b0d585d79e4b79710c6154b710b2e826637402f5
c1949969 <wilson-hallj@cardiff.ac.uk>
2021-03-08 17:42:34 +0000

    hidden API key

--- SteamAPIHours/Main.cs
+++ SteamAPIHours/Main.cs
@@ -1,27 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace SteamAPIHours
 {
     public partial class Main : Form
     {
         //-------------------------Objects-----------------------------
-        private string steamProfileID = "76561198199033779";
-        private string steamAPI2WeeksCall = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=4855D3D95FA2A9484B68297725A23814&steamid=";
+        private string steamProfileID = "&steamid=76561198199033779";
+        private string steamAPI2WeeksCall = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=";
         private string steamAPIFormat = "&format=json";
+        private string APIKey = "KVkih8ZbA30e4ePOvMXGgxcFGdUN5rFIcAHgEJmRvz4=";
+        const string PasswordHash = "J&&Uj1kl";
+        const string SaltKey = "J$JI&KEY";
+        const string VIKey = "#6V9j4K0y1H4l3M2";
         Root jData = new Root();
         //-------------------------Objects-----------------------------
 
@@ -43,6 +41,34 @@ namespace SteamAPIHours
             }
         }
 
+        public static string Decrypt(string encryptedText)
+        {
+            if (encryptedText.Length % 4 != 0)
+            {
+                return encryptedText;
+            }
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+            try
+            {
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+            }
+            catch (CryptographicException)
+            {
+
+                return null;
+            }
+        }
+
         private void linkSteamIDFinder_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start("https://steamidfinder.com/");
@@ -50,18 +76,18 @@ namespace SteamAPIHours
 
         private void btnGo_Click(object sender, EventArgs e)
         {
-            steamProfileID = txtSteamID.Text;
+            steamProfileID = "&steamid="+txtSteamID.Text;
             GameHoursChart.Series["Hours"].Points.Clear();
             HttpClient client = new HttpClient();
             // Add an Accept header for JSON format.
             client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage response = client.GetAsync(steamAPI2WeeksCall + steamProfileID + steamAPIFormat).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            HttpResponseMessage response = client.GetAsync(steamAPI2WeeksCall +Decrypt(APIKey)+ steamProfileID + steamAPIFormat).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
             if (response.IsSuccessStatusCode)
             {
                 // Parse the response body.
-                var dataObjects = response.Content.ReadAsStringAsync();  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                var dataObjects = response.Content.ReadAsStringAsync();
                 deserialiseJSON(dataObjects.Result);
             }
             else
